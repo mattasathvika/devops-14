7338b39e50c92d4b39017a958ae04497a48068a3
mattasathvika <mattasathvika984@gmail.com>
2024-07-18 15:43:39 +0530

    Application.java file added

---
+++ Application.java
@@ -0,0 +1,11 @@
+public class CreateWebSource
+{
+  public static void main(String[] args) throws Exception
+  {
+    System.out.println( "" );
+ 
+    try
+    {
+      if ( args == null || args.length != 4 )
+      {
+       
